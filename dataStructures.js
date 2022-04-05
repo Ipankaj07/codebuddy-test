97c3265981dbc1fbd217d5c8f4893eea768c460d
Pankaj Raj <praj4936@gmail.com>
2022-04-05 21:51:28 +0530

    Update dataStructures.js

--- dataStructures.js
+++ dataStructures.js
@@ -1,6 +1,27 @@
 // level {1/2/3}
 
 //level 2
+// level {1/2/3}
+
+//level 2
+
+//1 Write a function to remove all even numbers from an Array.
+let array = [1, 3, 4, 6, 7, 8]
+
+function removeEven(arr) {
+    let res = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 !== 0) {
+            res.push(arr[i]);
+        }
+    }
+    return res;
+}
+
+console.log(removeEven(array));
+
+
+//2 Replace all the vowels in a string with uppercase vowels.
 
 let str = 'Elie'
 
@@ -23,3 +44,20 @@ function changeVowel(str) {
 
 
 console.log(changeVowel(str))
+
+
+//3. Write a function to find the maximum number in an array.
+
+let arr =  [1, 3, 4, 6, 7, 8, 2, 5]
+
+function findMax(arr){
+    let max = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+console.log(findMax(arr));
